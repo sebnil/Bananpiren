80259ee0b7ed8abe9326e8a78fe693aa68136040
Sebastian Nilsson <sebnil@gmail.com>
2016-07-21 00:00:59 +0200

    kinda playable.
    Former-commit-id: e39a20554e379a80093ea603711b8bf03c48c74d

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazard;
+
+    public Vector3 spawnValues;
+
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public int hazardCount;
+
+    public int xMean;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnWaves());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                for (int port = 0; port < 2; port++)
+                {
+                    int swapSide = 1;
+                    if (port == 0)
+                    {
+                        swapSide = -1;
+                    }
+                    Vector3 spawnPosition = new Vector3(Random.Range(swapSide*xMean - spawnValues.x, swapSide*xMean + spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazard, spawnPosition, spawnRotation);
+                }
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
