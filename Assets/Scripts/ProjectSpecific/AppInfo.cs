7643275ab62d2e6935832323285207f8f37db779
Sebastian Nilsson <sebnil@gmail.com>
2017-01-17 18:27:46 +0100

    making it a bit easier because my brother is whiner and not a gamer.

--- Assets/Scripts/ProjectSpecific/AppInfo.cs
+++ Assets/Scripts/ProjectSpecific/AppInfo.cs
@@ -54,7 +54,7 @@ public static class AppInfo
     /// </summary>
     public static string minorVersion
     {
-        get { return "26"; }
+        get { return "27"; }
     }
 
     /// <summary>
