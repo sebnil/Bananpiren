160f67eae21cdf9b26199fdc6cb06da5b85963ae
Sebastian Nilsson <sebnil@gmail.com>
2017-05-22 22:54:49 +0200

    include projectpath in build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 :: https://github.com/Chaser324/unity-build/wiki/Command-Line-Interface
-"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -nographics -executeMethod SuperSystems.UnityBuild.BuildCLI.PerformBuild
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -nographics -projectPath "%cd%" -executeMethod SuperSystems.UnityBuild.BuildCLI.PerformBuild
