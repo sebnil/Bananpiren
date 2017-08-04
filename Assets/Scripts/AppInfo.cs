38217224d48ef07ea0febe47574dc57d367afd2d
Sebastian Nilsson <sebnil@gmail.com>
2017-08-04 13:48:37 +0200

    Disable debug mode. Bump version to v1.03

--- Assets/Scripts/AppInfo.cs
+++ Assets/Scripts/AppInfo.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 public static class AppInfo
 {
-    public static bool DebugRelease = true; // no ability for time to run out, show buttons to give more time etc
+    public static bool DebugRelease = false; // no ability for time to run out, show buttons to give more time etc
     public static bool SemiDebugRelease = true; // show native logger
     public static bool EnableRemoteLogging = true; // send log files to google spreadsheet
 }
